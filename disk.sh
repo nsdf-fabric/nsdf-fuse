2c7d2bc9b74bce2912d3a0d3ac35ba29bbe6a817
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-03-12 13:13:33 -0700

    working

--- disk.sh
+++ disk.sh
@@ -70,22 +70,7 @@ function RunDiskTest() {
 
     if [[ "${FAST}" == "1" ]] ; then
 
-        # one sequential (ilesize*numjobs=tot-storage=64M   size=fuse-activity=64M)
-        RunFioWriteTest --name=one-seq-write   --rw=write --bs=4M --filesize=64M --numjobs=1     --size=64M
-        RunFioReadTest  --name=one-seq-read    --rw=read  --bs=4M --filesize=64M --numjobs=1     --size=64M 
-        rm -Rf $TEST_DIR/* 
-
-        # multi sequential
-        RunFioWriteTest --name=multi-seq-write --rw=write --bs=4M  --filesize=1M  --numjobs=64   --size=64M 
-        RunFioReadTest  --name=multi-seq-read  --rw=read  --bs=4M  --filesize=1M  --numjobs=64   --size=64M
-        rm -Rf $TEST_DIR/*
-
-        # rand test (filesize*numjobs=tot-storage=64M      numjobs*size=fuse-activity=64M) 
-        RunFioWriteTest --name=rand-write --rw=randwrite  --bs=64k  --filesize=2M --numjobs=32   --size=2M
-        RunFioReadTest  --name=rand-read  --rw=randread   --bs=64k  --filesize=2M --numjobs=32   --size=2M 
-        rm -Rf $TEST_DIR/* 
-
-        RunTarTest       
+        RunFioWriteTest --name=one-seq-write   --rw=write --bs=4M --filesize=64M --numjobs=1 --size=64M     
         rm -Rf $TEST_DIR/*  
         
     else
