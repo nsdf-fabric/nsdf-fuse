cbee419e22ac833c114ea25592fe81013298b8fd
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-03-13 23:20:24 -0600

    working

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,14 @@
 # Instructions
 
-Define the credentials for the S3 backend (change as needed):
+Update OS and install dependencies:
+
+```
+sudo apt -qq update
+sudo apt -qq install -y nload expect python3 python3-pip awscli fuse libfuse-dev 
+```
+
+
+Define the credentials for the the tests (change as needed):
 
 ```
 export ACCESS_KEY=XXXXX
@@ -14,67 +22,166 @@ export OBJECTIVEFS_LICENSE=ZZZZZ
 # you must create a "File System" (see https://juicefs.com/console/)
 # with name `nsdf-fuse-test-juicefs`
 export JUICE_TOKEN=KKKKK
+```
 
-# uncomment this if you want to just test quickly the scripts
-# export FAST=1
+Install lastest versio of `fio`:
+
+```
+git clone https://github.com/axboe/fio
+pushd fio
+./configure
+make 
+sudo make install
+sudo cp /usr/local/bin/fio /usr/bin/fio 
+popd
+rm -Rf fio
+```
+
+Install `geesefs` and create credentials:
+
+```
+wget https://github.com/yandex-cloud/geesefs/releases/latest/download/geesefs-linux-amd64
+sudo mv geesefs-linux-amd64 /usr/bin/geesefs
+chmod a+x /usr/bin/geesefs
+
+mkdir -p ${HOME}/.aws
+cat << EOF > ${HOME}/.aws/credentials
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+EOF
 
 ```
 
-Run one of the test:
+Install `goofys`:
 
 ```
-git clone https://github.com/nsdf-fabric/nsdf-fuse-test
-cd nsdf-fuse-test
+wget https://github.com/kahing/goofys/releases/latest/download/goofys
+sudo mv goofys /usr/bin/
+chmod a+x /usr/bin/goofys
+```
+
+Install `juicefs`:
+
+```
+wget -q https://juicefs.com/static/juicefs
+sudo mv juicefs /usr/bin
+chmod +x /usr/bin/juicefs 
+```
+
+Also for `juicefs` you need to create a File System named `juicefs-nsdf-fuse-test-juicefs` (see https://juicefs.com/console/).
 
 
-export AWS_ACCESS_KEY_ID=XXXXX
-export AWS_SECRET_ACCESS_KEY=YYYYY
-export AWS_DEFAULT_REGION=us-east-1
-export OBJECTIVEFS_LICENSE=ZZZZZ     # only if you test objectivefs
-export JUICE_TOKEN=KKKKK             # only if you test juice
+Install `objectivefs` and create credentials:
 
-seq-1-write
-seq-1-read
-seq-n-write
-seq-n-read
-rnd-n-write
-rnd-n-read
-tar-gz
-rm-files
+```
+wget -q https://objectivefs.com/user/download/asn7gu3nd/objectivefs_6.9.1_amd64.deb
+sudo dpkg -i objectivefs_6.9.1_amd64.deb
+rm objectivefs_6.9.1_amd64.deb
+
+sudo mkdir -p /etc/objectivefs.env
+sudo rm -Rf /etc/objectivefs.env/*
+sudo bash -c "echo ${AWS_ACCESS_KEY_ID}      > /etc/objectivefs.env/AWS_ACCESS_KEY_ID"
+sudo bash -c "echo ${AWS_SECRET_ACCESS_KEY}  > /etc/objectivefs.env/AWS_SECRET_ACCESS_KEY"
+sudo bash -c "echo ${AWS_DEFAULT_REGION}     > /etc/objectivefs.env/AWS_DEFAULT_REGION"
+sudo bash -c "echo ${OBJECTIVEFS_LICENSE}    > /etc/objectivefs.env/OBJECTIVEFS_LICENSE"
+sudo bash -c "echo ${OBJECTIVEFS_LICENSE}    > /etc/objectivefs.env/OBJECTIVEFS_PASSPHRASE"
+sudo chmod 600 /etc/objectivefs.env/*
+```
+
+Install `rclone` and set credentials:
+
+```
+wget https://downloads.rclone.org/v1.57.0/rclone-v1.57.0-linux-amd64.deb
+sudo dpkg -i rclone-v1.57.0-linux-amd64.deb
+rm rclone-v1.57.0-linux-amd64.deb
+
+cat << EOF >> ~/.config/rclone/rclone.conf
+[nsdf-test-rclone]
+type=s3
+provider=Other
+access_key_id=${AWS_ACCESS_KEY_ID}
+secret_access_key=${AWS_SECRET_ACCESS_KEY}
+region=${AWS_DEFAULT_REGION} 
+endpoint=https://s3.${AWS_DEFAULT_REGION}.amazonaws.com
+EOF
+chmod 600 ~/.config/rclone/rclone.conf
+```
+
+Install `s3backer`:
+
+```
+sudo apt install -y s3backer
+sudo sh -c 'echo user_allow_other >> /etc/fuse.conf'
+```
+
+Install `s3fs` and setup credentials:
+
+```
+sudo apt install -y s3fs 
+
+echo ${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY} > ${HOME}/.s3fs
+chmod 600 ${HOME}/.s3fs
+```
+
+Install `s3ql` and setup credentials (NOTE fuse3 is not compatible with fuse so you will need to run this test alone):
+
+```
+sudo apt install -y sqlite3 libsqlite3-dev pkg-config libfuse3-dev
+# sudo apt remove fuse 
+# sudo apt install -y fuse3 
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade pyfuse3 google-auth-oauthlib dugong apsw defusedxml trio
+
+wget https://github.com/s3ql/s3ql/releases/download/release-3.8.1/s3ql-3.8.1.tar.gz
+tar xzf s3ql-3.8.1.tar.gz
+pushd s3ql-3.8.1
+python3 setup.py build_ext --inplace
+sudo python3 setup.py install 
+popd
+sudo rm -Rf s3ql-3.8.1.tar.gz s3ql-3.8.1
+
+mkdir -p ${HOME}/.s3ql/
+cat << EOF > ~/.s3ql/authinfo2
+[s3-test]
+storage-url: s3://${AWS_DEFAULT_REGION}/${BUCKET_NAME}
+backend-login: ${AWS_ACCESS_KEY_ID}
+backend-password: ${AWS_SECRET_ACCESS_KEY}
+EOF
+chmod 600 ~/.s3ql/authinfo2
+```
+
+# Run test
 
 
-./geesefs.sh     > output.1.geesefs      2>&1
-./goofys.sh      > output.1.goofys       2>&1  
-./juicefs.sh     > output.1.juicefs      2>&1
-./objectivefs.sh > output.1.objectivefs  2>&1
-./rclone.sh      > output.1.rclone       2>&1
-./s3backer.sh    > output.1.s3backer     2>&1
-./s3fs.sh        > output.1.s3fs         2>&1
-./s3ql.sh        > output.1.s3ql         2>&1
+# TODO
 
-# you can extract the part this way
-for f in geesefs goofys juicefs objectivefs rclone s3backer s3fs s3ql; do
-   echo $f
-   grep " write: IOPS\| read: IOPS\|real " output.1.$f  | cut -d" " -f2,3,4,5 
-   echo
+Run tests:
+
+```
+
+# TODO s3ql
+
+for it in geesefs goofys juicefs objectivefs rclone s3backer s3fs ; do
+   ./test.sh $it create-bucket
+
+   ./test.sh geesefs seq-1-write
+   ./test.sh geesefs seq-1-read
+   ./test.sh geesefs clean-bucket
+
+   ./test.sh geesefs seq-n-write
+   ./test.sh geesefs seq-n-read
+   ./test.sh geesefs clean-bucket
+
+   ./test.sh geesefs rnd-n-write
+   ./test.sh geesefs rnd-n-read
+   ./test.sh geesefs clean-bucket
+
+   ./test.sh geesefs tar-gz
+   ./test.sh geesefs clean-bucket
+
+   ./test.sh $it remove-bucket
+
 done
 ```
 
-
-# Misc:
-
-To clean up broken test:
-
-```
-for it in $(mount | grep mount)
-do 
-    echo $it
-done
-
-rm -Rf ~/mount
-
-for it in $(aws s3 ls | cut -d" " -f3)
-do 
-    aws s3 rb s3://$it --force 
-done
-``
---
+++ test.sh
@@ -0,0 +1,392 @@
+#!/bin/bash
+set -e # exit when any command fails
+
+# example:
+# test.sh geesefs create-bucket
+SOFTWARE=$1
+TEST_NAME=$2
+
+# ///////////////////////////////////////////////////////////
+function BaseCreateBucket() {
+    echo "BaseCreateBucket name=$1 region=$2..."
+    aws s3 mb s3://${1} --region ${2}
+}
+
+# ///////////////////////////////////////////////////////////
+function BaseRemoveBucket() {
+    echo "BaseRemoveBucket ${1}..."
+    aws s3 rb --force s3://${1}
+    echo "BaseRemoveBucket ${1} done"
+}
+
+# ///////////////////////////////////////////////////////////
+function CreateBucket() {
+    BaseCreateBucket ${BUCKET_NAME} ${AWS_DEFAULT_REGION}
+}
+
+# ///////////////////////////////////////////////////////////
+function RemoveBucket() {
+    BaseRemoveBucket ${BUCKET_NAME}
+}
+
+# ///////////////////////////////////////////////////////////
+function FuseUp() {
+    echo "ERROR FuseUp To override"
+    exit 1
+}
+
+# ///////////////////////////////////////////////////////////
+function FuseDown() {
+    # first I unmount and then I remove all the cached (like I did a reboot)
+    sudo umount ${TEST_DIR} 
+    rm -Rf ${BASE_DIR}
+}
+
+# ///////////////////////////////////////////////////////////////
+# see https://docs.weka.io/v/3.10/testing-and-troubleshooting/testing-weka-system-performance
+function RunFioTest() {
+    fio $@ \
+        --directory=${TEST_DIR} \
+        --filename_format='$jobnum/$filenum/test.$jobnum.$filenum.bin' \
+        --ioengine=posixaio \
+        --exitall_on_error=1 \
+        --create_serialize=0 \
+        --end_fsync=1 \
+        --disk_util=0 \
+        --group_reporting \
+        --ramp_time=2s \
+        --direct=1
+}
+
+
+# ////////////////////////////////////////////////////////////////////////////////
+# see https://github.com/yandex-cloud/geesefs
+if [[  "${SOFTWARE}" == "geesefs" ]] ; then
+
+    function FuseUp() {
+        geesefs \
+            --cache ${CACHE_DIR} \
+            --memory-limit ${DISK_CACHE_SIZE_MB} \
+            --log-file ${LOG_DIR}/log.txt \
+            --no-checksum \
+            --max-flushers 32 \
+            --max-parallel-parts 32 \
+            --part-sizes 25 \
+            --endpoint https://s3.${AWS_DEFAULT_REGION}.amazonaws.com \
+            ${BUCKET_NAME} \
+            ${TEST_DIR} || true # the command does not return 0 (weird)
+        mount | grep ${TEST_DIR}
+    }
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+if [[  "${SOFTWARE}" == "goofys" ]] ; then
+
+    function FuseUp() {
+        goofys \
+            --region ${AWS_DEFAULT_REGION} \
+            ${BUCKET_NAME} \
+            ${TEST_DIR}
+        mount | grep ${TEST_DIR}
+    }
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+# IMPORTANT: internally the real bucket name will be juicefs-${BUCKET_NAME}
+if [[  "${SOFTWARE}" == "juicefs" ]] ; then
+
+    function CreateBucket() {
+        juicefs auth \
+            ${BUCKET_NAME} \
+            --token ${JUICE_TOKEN} \
+            --accesskey ${AWS_ACCESS_KEY_ID} \
+            --secretkey ${AWS_SECRET_ACCESS_KEY}  
+    }
+
+    function RemoveBucket() {
+        # note: there is a prefix
+        BaseRemoveBucket juicefs-${BUCKET_NAME}
+    }
+
+    function FuseUp() {
+        juicefs mount \
+            ${BUCKET_NAME} \
+            ${TEST_DIR} \
+            --cache-dir=${CACHE_DIR} \
+            --log=${LOG_DIR}/log.log \
+            --max-uploads=150 \
+            --cache-size=${DISK_CACHE_SIZE_MB}
+        mount | grep ${TEST_DIR}
+    }
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+# see https://objectivefs.com/howto/performance-amazon-efs-vs-objectivefs-large-files
+if [[  "${SOFTWARE}" == "objectivefs" ]] ; then
+
+    function CreateBucket() {
+        cat << EOF > create_bucket.sh
+#!/usr/bin/expect -f
+set timeout -1
+spawn mount.objectivefs create -l ${AWS_DEFAULT_REGION} ${1}
+match_max 100000
+expect -exact "for s3://${1}): "
+send -- "${OBJECTIVEFS_LICENSE}\r"
+expect eof
+EOF
+        sudo chmod 700 create_bucket.sh
+        sudo ./create_bucket.sh
+        rm create_bucket.sh
+    }
+
+    function FuseUp() {
+        export  DISKCACHE_SIZE=${DISK_CACHE_SIZE_MB}M
+        export  DISKCACHE_PATH=${CACHE_DIR}
+        export  CACHESIZE=${RAM_CACHE_SIZE_MB}
+        sudo mount.objectivefs \
+            -o mt \
+            s3://${BUCKET_NAME} \
+            ${TEST_DIR}
+        sudo mount | grep ${TEST_DIR}
+        sudo chmod a+rwX -R ${TEST_DIR}
+    }
+fi
+
+if [[  "${SOFTWARE}" == "rclone" ]] ; then
+    function FuseUp() {
+        rclone mount \
+            nsdf-test-rclone:${BUCKET_NAME} \
+            ${TEST_DIR} \
+            --uid $UID \
+            --daemon \
+            --vfs-cache-mode writes \
+            --use-server-modtime \
+            --cache-dir ${CACHE_DIR} \
+            --vfs-cache-mode minimal 
+        mount | grep ${TEST_DIR}
+    }
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+# Explanation:
+#   Linux loop back mount
+#   s3backer <---> remote S3 storage
+if [[  "${SOFTWARE}" == "s3backer" ]] ; then
+
+    function MountBackend() {
+        mkdir -p ${CACHE_DIR}/backend
+        for i in 1 2 3 4 5; do 
+            s3backer \
+                --accessId=${AWS_ACCESS_KEY_ID} \
+                --accessKey=${AWS_SECRET_ACCESS_KEY} 
+                --region=${AWS_DEFAULT_REGION}  \
+                --blockCacheFile=${CACHE_DIR}/block_cache_file \
+                --blockSize=4M \
+                --size=1T \
+                --blockCacheThreads=64  \
+                --blockCacheSize=$(( ${RAM_CACHE_SIZE_MB} / 4 )) \
+                ${BUCKET_NAME} ${CACHE_DIR}/backend  && break
+            sleep 1
+        done
+        mount | grep ${CACHE_DIR}
+    }
+
+    function CreateBucket() {
+        BaseCreateBucket ${BUCKET_NAME} ${AWS_DEFAULT_REGION}
+        MountBackend
+        mkfs.ext4 \
+            -E nodiscard \
+            -F ${CACHE_DIR}/backend/file
+        umount ${CACHE_DIR}/backend
+    }
+
+    function FuseUp(){
+        MountBackend
+        sudo mount \
+            -o loop \
+            -o discard \
+            ${CACHE_DIR}/backend/file \
+            ${TEST_DIR}  
+        mount | grep ${TEST_DIR}
+        sudo chmod a+rwX -R ${TEST_DIR}
+    }
+
+    function FuseDown() {
+        sudo umount ${TEST_DIR}
+        sudo umount ${CACHE_DIR}/backend
+        rm -Rf ${BASE_DIR}
+    }
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+# see http://manpages.ubuntu.com/manpages/bionic/man1/s3fs.1.html
+# see https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-s3fs
+if [[  "${SOFTWARE}" == "s3fs" ]] ; then
+
+    function FuseUp() {
+        s3fs \
+            ${BUCKET_NAME} \
+            ${TEST_DIR} \
+            -o passwd_file=${HOME}/.s3fs \
+            -o endpoint=${AWS_DEFAULT_REGION} \
+            -o use_cache=${CACHE_DIR} \
+            -o cipher_suites=AESGCM \
+            -o max_background=1000 \
+            -o max_stat_cache_size=100000 \
+            -o multipart_size=52 \
+            -o parallel_count=30 \
+            -o multireq_max=30 \
+            -o allow_other  
+        mount | grep ${TEST_DIR}
+    }
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+# https://www.rath.org/s3ql-docs/man/mkfs.html
+# https://www.rath.org/s3ql-docs/man/mount.html
+if [[  "${SOFTWARE}" == "s3ql" ]] ; then
+
+    function CreateBucket() {
+        BaseCreateBucket ${BUCKET_NAME} ${AWS_DEFAULT_REGION}
+        mkfs.s3ql \
+            --cachedir ${CACHE_DIR} \
+            --log ${LOG_DIR}/log \
+            --plain \
+            s3://${AWS_DEFAULT_REGION}/${BUCKET_NAME}
+    }
+
+    function FuseUp() {
+        for i in 1 2 3 4 5; do 
+            mount.s3ql \
+                --cachedir ${CACHE_DIR} \
+                --log ${LOG_DIR}/log \
+                --cachesize $(( ${DISK_CACHE_SIZE_MB} * 1024 )) \
+                s3://${AWS_DEFAULT_REGION}/${BUCKET_NAME} \
+                ${TEST_DIR}  && break
+            sleep 1
+        done
+        mount | grep ${TEST_DIR}
+        echo "FuseUp (s3ql) done"
+    }
+
+    function FuseDown() {
+        umount.s3ql \
+            --log ${LOG_DIR}/log \
+            ${TEST_DIR}
+        rm -Rf ${BASE_DIR}
+    }
+
+fi
+
+# ////////////////////////////////////////////////////////////////////////////////
+# check traffic by `sudo nload -u M -U M`
+
+BUCKET_NAME=nsdf-fuse-test-${SOFTWARE}
+
+export BASE_DIR=${HOME}/mount/buckets/${BUCKET_NAME}
+export TEST_DIR=${BASE_DIR}/test
+export CACHE_DIR=${BASE_DIR}/cache
+export LOG_DIR=${BASE_DIR}/log
+export DISK_CACHE_SIZE_MB=1024 # CACHE SIZE IN MB, make it small so that numbers are not affected too much by disk cache
+export RAM_CACHE_SIZE_MB=1024 # CACHE SIZE IN MB, make it small so that numbers are not affected too much by disk cache
+
+echo "BUCKET_NAME:        ${BUCKET_NAME}"
+echo "AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}"
+echo "DISK_CACHE_SIZE_MB: ${DISK_CACHE_SIZE_MB}"
+echo "RAM_CACHE_SIZE_MB:  ${RAM_CACHE_SIZE_MB}"
+echo "BASE_DIR:           ${BASE_DIR}"
+echo "TEST_DIR:           ${TEST_DIR}"
+echo "CACHE_DIR:          ${CACHE_DIR}"
+echo "LOG_DIR:            ${LOG_DIR}"
+
+echo "Starting TEST_NAME=${TEST_NAME} TEST_DIR=${TEST_DIR}..." 
+
+# minimize effect of RAM cache
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches" 
+
+mkdir  -p ${BASE_DIR}  || true
+mkdir  -p ${TEST_DIR}  || true
+mkdir  -p ${CACHE_DIR} || true
+mkdir  -p ${LOG_DIR}   || true
+
+if [[ "${TEST_NAME}" == "create-bucket" ]] ; then
+    CreateBucket
+    aws s3 ls  
+
+elif [[ "${TEST_NAME}" == "remove-bucket" ]] ; then
+    RemoveBucket 
+    aws s3 ls     
+
+elif [[ "${TEST_NAME}" == "clean-bucket" ]] ; then
+    SECONDS=0
+    FuseUp
+    time -p rm -Rf ${TEST_DIR}/* 
+    FuseDown
+    echo "${TEST_NAME} done. Seconds: $SECONDS"
+
+else
+
+    OPTION_W="--allow_file_create=1  --end_fsync=1 --refill_buffers --create_serialize=0 --fallocate=none"
+    OPTION_R="--allow_file_create=0"
+
+    # tot_storage=filesize*numjobs=64G fuse-activity=size=64G
+    if [[ "${TEST_NAME}" == "seq-1-write" ]] ; then
+        SECONDS=0
+        FuseUp
+        RunFioTest --name=seq-1-write   --rw=write --bs=4M --filesize=64G --numjobs=1     --size=64G ${OPTION_W} || true
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS"
+
+    elif [[ "${TEST_NAME}" == "seq-1-read" ]] ; then
+        SECONDS=0
+        FuseUp
+        RunFioTest  --name=seq-1-read    --rw=read  --bs=4M --filesize=64G --numjobs=1     --size=64G ${OPTION_R} || true
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS"
+
+    # multi sequential (tot-storage=filesize*numjobs=64G fuse-activity=size=64G)
+    elif [[ "${TEST_NAME}" == "seq-n-write" ]] ; then
+        SECONDS=0
+        FuseUp
+        RunFioTest --name=seq-n-write --rw=write --bs=4M  --filesize=1G  --numjobs=64   --size=64G ${OPTION_W} || true
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS"
+
+    elif [[ "${TEST_NAME}" == "seq-n-read" ]] ; then
+        SECONDS=0
+        FuseUp
+        RunFioTest  --name=seq-n-read  --rw=read  --bs=4M  --filesize=1G  --numjobs=64   --size=64G ${OPTION_R}  || true
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS"
+
+    # rand test (tot-storage=filesize*numjobs=64G fuse-activity=numjobs*size=8G) (WEIRD: rand test use --size with a different meaning)
+    elif [[ "${TEST_NAME}" == "rnd-n-write" ]] ; then
+        SECONDS=0
+        FuseUp
+        RunFioWriteTest --name=rnd-n-write --rw=randwrite  --bs=64k  --filesize=2G --numjobs=32   --size=256M ${OPTION_W} || true
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS" 
+
+    elif [[ "${TEST_NAME}" == "rnd-n-read" ]] ; then
+        SECONDS=0
+        FuseUp
+        RunFioTest  --name=rnd-n-read  --rw=randread   --bs=64k  --filesize=2G --numjobs=32   --size=256M ${OPTION_R}  || true
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS"
+
+    elif [[ "${TEST_NAME}" == "tar-gz" ]] ; then
+        wget  https://curl.se/download/curl-7.82.0.tar.gz 
+        SECONDS=0
+        FuseUp
+        time -p tar xzf curl-7.82.0.tar.gz 1>/dev/null -C ${TEST_DIR} 
+        FuseDown
+        echo "${TEST_NAME} done. Seconds: $SECONDS"
+        rm -f curl-7.82.0.tar.gz
+
+    else
+        echo "UNKNOWN TEST"
+        exit 1
+    fi
+fi
+
+echo "All done"
