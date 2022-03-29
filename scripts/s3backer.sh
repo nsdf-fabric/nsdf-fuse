5d556505a85b2c639d57b76589cefe811a22879a
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-03-29 06:30:46 -0700

    Fixing endpoint

--- scripts/s3backer.sh
+++ scripts/s3backer.sh
@@ -12,9 +12,7 @@
 function Install_s3backer() {
 	sudo apt install -y s3backer
 	sudo sh -c 'echo user_allow_other >> /etc/fuse.conf'
-
-	# check the version
-	s3backer --version	
+	s3backer --version	# check the version
 }
 
 # //////////////////////////////////////////////////////////////////////////
@@ -26,35 +24,45 @@ function Uninstall_s3backer() {
 # //////////////////////////////////////////////////////////////////
 function MountBackend() {
     echo "MountBackend  s3backer..."
+
     mkdir -p ${CACHE_DIR}/backend
-	 OVERALL_SIZE=256G
+	
+    # I need to know the size in advance (not good!)
+    OVERALL_SIZE=256G
 
 	# disabling caching
-   # --blockCacheFile=${CACHE_DIR}/block_cache_file
+    # --blockCacheFile=${CACHE_DIR}/block_cache_file
 	# --blockCacheSize=???
-   # --blockCacheThreads=64  
-    Retry s3backer \
+    # --blockCacheThreads=64  
+    # --debug-http
+    Retry s3backer  \
             --accessId=${AWS_ACCESS_KEY_ID} \
             --accessKey=${AWS_SECRET_ACCESS_KEY} \
             --region=${AWS_DEFAULT_REGION}  \
             --blockSize=4M \
             --size=$OVERALL_SIZE \
-				--endpoint=${AWS_S3_ENDPOINT_URL} \
-            ${BUCKET_NAME} ${CACHE_DIR}/backend
+			--baseURL=${AWS_S3_ENDPOINT_URL}/  \
+            ${BUCKET_NAME} \
+            ${CACHE_DIR}/backend
+
     CheckMount ${CACHE_DIR}/backend
+    
     echo "MountBackend  s3backer done"
 }
 
 
 # //////////////////////////////////////////////////////////////////
 function CreateBucket() {
-    echo "CreateBucket  s3backer..."
-    aws s3api create-bucket --bucket ${BUCKET_NAME} --region ${AWS_DEFAULT_REGION}
+    echo "CreateBucket  (s3backer)..."
+    BaseCreateBucket ${BUCKET_NAME}
+
+    # i need to format the "disk"
     MountBackend
     mkfs.ext4 \
         -E nodiscard \
         -F ${CACHE_DIR}/backend/file
     Retry umount ${CACHE_DIR}/backend
+
     echo "CreateBucket s3backer done"
 }
 
@@ -65,7 +73,7 @@ function FuseUp(){
     Retry mkdir -p ${TEST_DIR}
     MountBackend
     sudo mount -o loop  -o discard ${CACHE_DIR}/backend/file ${TEST_DIR}  
-	 CheckMount ${TEST_DIR}
+	CheckMount ${TEST_DIR}
     sudo chmod a+rwX -R ${TEST_DIR}
     echo "FuseUp s3backer done"
 }
