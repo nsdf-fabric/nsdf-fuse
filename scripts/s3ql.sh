f5a6ad58fad26e68973c83d23428e40801f72ef1
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-03-29 09:45:04 -0700

    Fixing s3ql

--- scripts/s3ql.sh
+++ scripts/s3ql.sh
@@ -3,8 +3,6 @@
 # https://www.rath.org/s3ql-docs/man/mkfs.html
 # https://www.rath.org/s3ql-docs/man/mount.html
 
-
-
 # //////////////////////////////////////////////////////////////////////////
 function Install_s3ql() {
 
@@ -28,16 +26,6 @@ function Install_s3ql() {
 	popd
 	sudo rm -Rf s3ql-3.8.1.tar.gz s3ql-3.8.1
 
-	# create credentials
-	mkdir -p ${HOME}/.s3ql/
-	cat << EOF > ~/.s3ql/authinfo2
-[s3-test]
-storage-url: s3://${AWS_S3_ENDPOINT_URL}/${BUCKET_NAME}
-backend-login: ${AWS_ACCESS_KEY_ID}
-backend-password: ${AWS_SECRET_ACCESS_KEY}
-EOF
-	chmod 600 ~/.s3ql/authinfo2
-
 	# check the version
 	mount.s3ql --version	
 }
@@ -53,17 +41,37 @@ function Uninstall_s3ql() {
 function CreateBucket() {
 
     echo "CreateBucket s3ql..."
-    aws s3api create-bucket --bucket ${BUCKET_NAME} --region ${AWS_DEFAULT_REGION} --endpoint-url ${AWS_S3_ENDPOINT_URL}
+	BaseCreateBucket ${BUCKET_NAME}
 
-    mkfs.s3ql \
+	
+	#  example: s3c://s3.us-west-1.wasabisys.com:443/${BUCKET_NAME} 
+
+
+	# start must be s3c (i.e. s3 compatible)
+	# rath.org/s3ql-ocs/backends.html
+	# NOTE: not sure if I need to add the 443 port
+	__endpoint__=${AWS_S3_ENDPOINT_URL//https/s3c}/${BUCKET_NAME}
+	
+	# credentials
+	mkdir -p ${HOME}/.s3ql/
+	cat << EOF > $HOME/.s3ql/authinfo2
+[nsdf-test]
+storage-url: ${__endpoint__}
+backend-login: ${AWS_ACCESS_KEY_ID}
+backend-password: ${AWS_SECRET_ACCESS_KEY}
+EOF
+	chmod 600 ~/.s3ql/authinfo2
+
+
+	# mount
+    mkfs.s3ql  \
+	    --authfile $HOME/.s3ql/authinfo2 \
         --cachedir ${CACHE_DIR} \
         --log ${LOG_DIR}/log \
         --plain \
-        s3://${AWS_S3_ENDPOINT_URL}/${BUCKET_NAME}
+        ${__endpoint__}
     echo "CreateBucket s3ql done"
 }
-
-
 
 # //////////////////////////////////////////////////////////////////
 function FuseUp() {
@@ -71,13 +79,16 @@ function FuseUp() {
     sync && DropCache
     mkdir -p ${TEST_DIR}
 
+	__endpoint__=${AWS_S3_ENDPOINT_URL//https/s3c}/${BUCKET_NAME}
+
 	 # --cachesize <size> Cache size in KiB (default: autodetect).
 	 # here I am setting to 64MiB to keep it minimal
     Retry mount.s3ql \
+	        --authfile $HOME/.s3ql/authinfo2 \
             --cachedir ${CACHE_DIR} \
             --log ${LOG_DIR}/log \
-				--cachesize $(( 64 * 1024 )) \
-            s3://${AWS_S3_ENDPOINT_URL}/${BUCKET_NAME} \
+            --cachesize $(( 64 * 1024 )) \
+            ${__endpoint__} \
             ${TEST_DIR} 
     
     CheckMount ${TEST_DIR}
